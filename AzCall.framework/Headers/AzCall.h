dd98ab2dc133c7ba61ce35f16de0aa7d569f0768
Luong Van Lam <lam.luongvan2305@gmail.com>
2018-06-18 12:25:45 +0700

    First init

---
+++ AzCall.framework/Headers/AzCall.h
@@ -0,0 +1,19 @@
+//
+//  AzCall.h
+//  AzCall
+//
+//  Created by Luong Van Lam on 5/9/18.
+//  Copyright © 2018 Luong Van Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AzCall.
+FOUNDATION_EXPORT double AzCallVersionNumber;
+
+//! Project version string for AzCall.
+FOUNDATION_EXPORT const unsigned char AzCallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AzCall/PublicHeader.h>
+
+
